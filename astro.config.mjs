790ed6a12c856227239c01a03af85c39277f2194
atavares2002 <atavares2002@gmail.com>
2025-12-31 12:22:03 -0300

    Atualizações cannonical url em layout, novo link do whatsapp com texto, astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,9 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://acessorios.polishmotors.com.br',
   vite: {
     plugins: [tailwindcss()]
   }
 });
+
